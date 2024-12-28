a31f94b733b1fc6a3099b45fd101eff5e9b2ac75
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-12-28 03:54:11 +0300

    add dto models

---
+++ MessageService/Models/Dto/MessageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageService.Models.Dto
+{
+    public class MessageDTO
+    {
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Text { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
