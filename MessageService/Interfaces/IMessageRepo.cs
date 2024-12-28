6c81f8d6acbd0733e3c1d3f6ccd5c22f0fc125f6
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-12-28 04:21:17 +0300

    create interface

--- MessageService/Interfaces/IMessageRepo.cs
+++ MessageService/Interfaces/IMessageRepo.cs
@@ -1,6 +1,10 @@
-﻿namespace MessageService.Interfaces
+﻿using MessageService.Models.Dto;
+
+namespace MessageService.Interfaces
 {
     public interface IMessageRepo
     {
+        int SendMessage(string text, string userFrom, string userTo);
+        List<MessageDTO> GetAllMessages(string userTo);
     }
 }
